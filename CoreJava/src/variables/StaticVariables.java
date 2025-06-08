6936f109c334ca923db3c715dd5687794a973c19
vivek <vivek.sirsath1@gmail.com>
2025-06-09 01:06:19 +0530

    In StaticVariables.java comments added for more understanding

--- CoreJava/src/variables/StaticVariables.java
+++ CoreJava/src/variables/StaticVariables.java
@@ -2,11 +2,13 @@ package variables;
 
 public class StaticVariables {
 	//Static Variable Creation
-	static String staticVariable = "I am a static Variable";
+	// If we declare the static variable as public. It can be accessed in another package too.
+	public static String staticVariable = "I am a static Variable";
 
 	public static void main(String[] args) {
 
-		//Calling Static Variable - Static variables are called directly without object instance of class
+		// Calling Static Variable - Static variables are called directly without object instance of class
+		// They can be called directly using Class name in any class in the same package.
 		System.out.println(StaticVariables.staticVariable);
 
 	}
