8f11dd28b9d2acbbca037bc5f23453d8315d8716
vivek <vivek.sirsath1@gmail.com>
2025-06-09 01:08:53 +0530

    ImportingStaticVariableFromAnotherPackage and printing it

---
+++ CoreJava/src/variables2/ImportingStaticVariableFromAnotherPackage.java
@@ -0,0 +1,10 @@
+package variables2;
+
+import variables.StaticVariables;
+
+public class ImportingStaticVariableFromAnotherPackage {
+
+	public static void main(String[] args) {
+		System.out.println(StaticVariables.staticVariable);
+	}
+}
